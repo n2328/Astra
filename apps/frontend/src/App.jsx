c5b7349e6e62fba95863aa65510fcba25cffd663
Aditi Suman <aditi.suman@ibm.com>
2026-07-10 20:37:16 +0530

    initialize Astra frontend

---
+++ apps/frontend/src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <h1
+      style={{
+        color: "white",
+        textAlign: "center",
+        marginTop: "100px",
+        fontSize: "48px",
+      }}
+    >
+      🚀 Astra
+    </h1>
+  );
+}
+
+export default App;
